13317e5993ebdde2fa27d9a3a9c0d9f62e2a204c
CrazyBallAdventure <123973434+CrazyBallAdventure@users.noreply.github.com>
2023-02-09 12:34:07 -0500

    remade mock profile site

---
+++ script.js
@@ -0,0 +1,23 @@
+function whodat() {
+    myname = prompt("Confirm Username for Access")
+    if (myname != 'Jane Doe') {
+        alert('Confirmed')
+    } else {
+        alert("Hello, " + myname);
+    }
+    
+}
+
+// applepie();
+
+// pie2(9);
+
+//  console.log(ingredientX);
+
+applepie();
+
+pie2(9);
+
+whodat();
+
+console.log(ingredientX);
